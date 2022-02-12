ce3e453675424d97b67dfe123b7d3a9f197b3de3
Arnav Parekhji <arnav17121999@gmail.com>
2022-02-12 15:35:53 -0800

    Lots of changes

---
+++ og_model.py
@@ -0,0 +1,55 @@
+from turtle import forward
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class OGNet(nn.Module):
+
+    def __init__(self, n_class):
+        super().__init__()
+        self.n_class = n_class
+        self.maxpool = nn.MaxPool2d(kernel_size=4, padding=1, stride=1)
+        self.enc1 = self.encoder_block(3,32)
+        self.enc2 = self.encoder_block(32,64)
+        self.enc3 = self.encoder_block(64,128)
+        self.enc4 = self.encoder_block(128,256)
+        self.dec1 = self.decoder_block(256,128)
+        self.dec2 = self.decoder_block(128,64)
+        self.dec3 = self.decoder_block(64,32)
+        self.classifier = nn.Conv2d(32, n_class, kernel_size=1)
+
+    def encoder_block(self, in_channels, out_channels):
+        block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=4, padding=1, dilation=(2,1)),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=4, padding=1, dilation=(2,1)),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU()
+        )
+        return block
+
+    def decoder_block(self, in_channels, out_channels):
+        block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=4, padding=1, dilation=(2,1)),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU,
+            nn.Conv2d(out_channels, out_channels, kernel_size=4, padding=1, dilation=(2,1)),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU,
+            nn.ConvTranspose2d(out_channels, out_channels, kernel_size=4, padding=1, stride=2, output_padding=1, dilation=(2,1))
+        )
+
+    def forward(self,x):
+        x1 = self.maxpool(self.enc1(x))
+        x2 = self.maxpool(self.enc2(x1))
+        x3 = self.maxpool(self.enc3(x2))
+        encoder_output = self.maxpool(self.enc4(x3))
+
+        y1 = self.dec1(encoder_output)
+        y2 = self.dec2(y1)
+        decoder_output = self.dec3(y2)
+
+        score = self.classifier(decoder_output)                   
+
+        return score
--- starter.py
+++ starter.py
@@ -1,4 +1,4 @@
-from cv2 import reduce
+from cv2 import inpaint, reduce
 from basic_fcn import *
 from dataloader import *
 from utils import *
@@ -9,6 +9,7 @@ import torch
 import gc
 from copy import deepcopy
 from zipfile import ZipFile
+import matplotlib.pyplot as plt
 
 # with ZipFile('tas500v1.1.zip','r') as zip_ref:
 #     zip_ref.extractall('')
@@ -28,8 +29,27 @@ def init_weights(m):
         torch.nn.init.xavier_uniform_(m.weight.data)
         torch.nn.init.normal_(m.bias.data) #xavier not applicable for biases   
 
+def dice_loss(input, target):
+    target = nn.functional.one_hot(target, num_classes = 10).permute(0,3,1,2).contiguous() 
+    smooth = 1.
+    
+    iflat = input.contiguous().view(-1)
+    tflat = target.contiguous().view(-1)
+    intersection = (iflat * tflat).sum()
+
+    A_sum = torch.sum(tflat * iflat)
+    B_sum = torch.sum(tflat * tflat)
+    
+    return 1 - ((2. * intersection + smooth) / (A_sum + B_sum + smooth) )
+
 epochs = 50       
-criterion = nn.CrossEntropyLoss(weight=None, size_average=None, ignore_index=-100, reduce=None, reduction="mean") # Choose an appropriate loss function from https://pytorch.org/docs/stable/_modules/torch/nn/modules/loss.html
+
+# # to use cross entropy loss
+# criterion = nn.CrossEntropyLoss(weight=None, size_average=None, ignore_index=-100, reduce=None, reduction="mean") # Choose an appropriate loss function from https://pytorch.org/docs/stable/_modules/torch/nn/modules/loss.html
+
+# to use dice coefficient loss
+criterion = dice_loss
+
 n_class = 10
 fcn_model = FCN(n_class=n_class)
 fcn_model.apply(init_weights)
@@ -40,23 +60,42 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 fcn_model = fcn_model.to(device) #transfer the model to the device
 best_model = None
 
+train_loss_epochs = []
+val_loss_epochs = []
+epoch_list = [i+1 for i in range(epochs)]
+
+class2color = {
+    0: (0.4117647058823529, 0.4117647058823529, 0.4117647058823529), \
+    1: (0,128,0), \
+    2: (0.9137254901960784, 0.5882352941176471, 0.47843137254901963), \
+    3: (0,  0,142), \
+    4: (0.0, 0.7490196078431373, 1.0),\
+    5: (255,255,0), \
+    6: (1,0,0), \
+    7: (1.0, 0.7137254901960784, 0.7568627450980392),\
+    8: (0.8627450980392157, 0.8627450980392157, 0.8627450980392157), \
+    9: (0,  0,  0)
+}
+
 def train():
     best_iou_score = 0.0
     for epoch in range(epochs):
         ts = time.time()
+        losses = []
         for iter, (inputs, labels) in enumerate(train_loader):
             # reset optimizer gradients
             optimizer.zero_grad()
 
             # both inputs and labels have to reside in the same device as the model's
-            inputs = inputs.to(device) #transfer the input to the same device as the model's
-            labels = labels.to(device) #transfer the labels to the same device as the model's
+            inputs = inputs.to(device)
+            labels = labels.to(device)
             labels = labels.to(dtype=torch.long)
             
             outputs = fcn_model(inputs) #we will not need to transfer the output, it will be automatically in the same device as the model's!
             
-            loss = criterion(outputs, labels) #calculate loss
-            
+            loss = criterion(outputs, labels)
+            losses.append(loss.item())
+
             # backpropagate
             loss.backward()
             # update the weights
@@ -66,15 +105,22 @@ def train():
                 print("epoch{}, iter{}, loss: {}".format(epoch, iter, loss.item()))
         
         print("Finish epoch {}, time elapsed {}".format(epoch, time.time() - ts))
-        
+        train_loss_epochs.append(np.mean(losses))
 
         current_miou_score = val(epoch)
         
         if current_miou_score > best_iou_score:
             best_iou_score = current_miou_score
             #save the best model
-            best_model = deepcopy(fcn_model)
+            best_model = torch.save(fcn_model.state_dict(),"best_model.pt")
             
+        plt.figure()
+        plt.plot(epoch_list, train_loss_epochs, label="Train Loss")
+        plt.plot(epoch_list, val_loss_epochs, label="Val Loss")
+        plt.xlabel("epochs")
+        plt.ylabel("train and val loss")
+        plt.legend()
+        plt.savefig("plots/train_val_loss.png")
         torch.cuda.empty_cache()
 
 def val(epoch):
@@ -89,25 +135,27 @@ def val(epoch):
         for iter, (input, label) in enumerate(val_loader):
 
             # both inputs and labels have to reside in the same device as the model's
-            input = input.to(device) #transfer the input to the same device as the model's
-            label = label.to(device) #transfer the labels to the same device as the model's
+            input = input.to(device)
+            label = label.to(device)
             output = fcn_model(input)
             
             label = label.to(dtype=torch.long)
             
-            loss = criterion(output, label) #calculate the loss
+            loss = criterion(output, label)
             losses.append(loss.item()) #call .item() to get the value from a tensor. The tensor can reside in gpu but item() will still work 
             predictions = nn.functional.softmax(output,dim=1)
-            pred = predictions.argmax(axis=1) # Make sure to include an argmax to get the prediction from the outputs of your model
+            pred = predictions.argmax(axis=1)
 
-            mean_iou_scores.append(np.nanmean(iou(pred, label, n_class)))  # Complete this function in the util, notice the use of np.nanmean() here
+            mean_iou_scores.append(np.nanmean(iou(pred, label, n_class)))
         
-            accuracy.append(pixel_acc(pred, label)) # Complete this function in the util
+            accuracy.append(pixel_acc(pred, label))
 
 
     print(f"Loss at epoch: {epoch} is {np.mean(losses)}")
     print(f"IoU at epoch: {epoch} is {np.mean(mean_iou_scores)}")
     print(f"Pixel acc at epoch: {epoch} is {np.mean(accuracy)}")
+
+    val_loss_epochs.append(np.mean(losses))
 
     fcn_model.train() #DONT FORGET TO TURN THE TRAIN MODE BACK ON TO ENABLE BATCHNORM/DROPOUT!!
 
@@ -118,32 +166,42 @@ def test():
     mean_iou_scores = []
     accuracy = []
 
+    best_model = FCN(n_class=n_class)
+    best_model = best_model.to(device)
+    best_model.load_state_dict(torch.load("best_model.pt"))
+
     #TODO: load the best model and complete the rest of the function for testing
     with torch.no_grad():
 
         for iter, (input, label) in enumerate(test_loader):
 
             # both inputs and labels have to reside in the same device as the model's
-            input = input.to(device) #transfer the input to the same device as the model's
-            label = label.to(device) #transfer the labels to the same device as the model's
+            input = input.to(device)
+            label = label.to(device)
             label = label.to(dtype=torch.long)
             
             output = best_model(input)
 
-            loss = criterion(output, label) #calculate the loss
+            loss = criterion(output, label)
             losses.append(loss.item())
-            pred = output.argmax(axis=1) # Make sure to include an argmax to get the prediction from the outputs of your model
+            pred = output.argmax(axis=1)
 
-            mean_iou_scores.append(np.nanmean(iou(pred, label, n_class)))  # Complete this function in the util, notice the use of np.nanmean() here
+            mean_iou_scores.append(np.nanmean(iou(pred, label, n_class)))
         
-            accuracy.append(pixel_acc(pred, label)) # Complete this function in the util
-
+            accuracy.append(pixel_acc(pred, label))
+    
+    # visualizing the segmented image
+    first_image = pred[0]
+    segmented_image = np.ones((384,768,3))
+    for cls in range(n_class):
+        segmented_image[first_image == cls] = class2color[cls]
+    plt.imsave("plots/test_segmented.png", segmented_image)
 
 if __name__ == "__main__":
     val(0)  # show the accuracy before training
     train()
     test()
-    
+
     # housekeeping
     gc.collect() 
     torch.cuda.empty_cache()
